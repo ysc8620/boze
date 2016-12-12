8b162f68a721ce3e8edee73f32e47107ff145959
ysc8620 <ysc8620@163.com>
2016-12-12 08:18:44 +0800

    ds

--- Application/Admin/Controller/RecordController.class.php
+++ Application/Admin/Controller/RecordController.class.php
@@ -12,22 +12,45 @@ class RecordController extends BaseController
      */
     public function index()
     {
-        $batch_id = I('request.batch',0,'intval');
+        $type = I('type', 0, 'intval');
+        $batch = I('batch',0,'intval');
+        $client = I('client',0,'intval');
+        $product = I('product',0,'intval');
 
-        $this->assign('batch_id', $batch_id);
+        $this->assign('type', $type);
+        $this->assign('batch_id', $batch);
+        $this->assign('client', $client);
+        $this->assign('product', $product);
 
         $model = M('product_record'); // 实例化User对象
         $where = [];
-        $where['type'] = 2;
-        if($batch_id){
-            $where['batch_id'] = $batch_id;
+
+        // 记录类型
+        if($type){
+            $where['type'] = $type;
         }
-        $count = $model->count();// 查询满足要求的总记录数
+
+        // 发货批次
+        if($batch){
+            $where['batch_no'] = $batch;
+        }
+
+        // 客户
+        if($client){
+            $where['client_id'] = $client;
+        }
+
+        // 箱子编号
+        if($product){
+            $where['product_name'] = $product;
+        }
+
+        $count = $model->where($where)->count();// 查询满足要求的总记录数
         $Page = new Page($count, 20);// 实例化分页类 传入总记录数和每页显示的记录数(25)
 
         $show = $Page->show();// 分页显示输出
         // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-        $list = $model->order('create_time')->order('id DESC')->limit($Page->firstRow . ',' . $Page->listRows)->select();
+        $list = $model->where($where)->order('create_time')->order('id DESC')->limit($Page->firstRow . ',' . $Page->listRows)->select();
         $this->assign('list', $list);// 赋值数据集
         $this->assign('page', $show);// 赋值分页输出
 
